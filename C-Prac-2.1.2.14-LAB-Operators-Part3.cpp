92acc49ec30fda9b53d7dbaf4a535f089ef8d557
EithanLlanos <eithanllanosb@gmail.com>
2023-11-13 11:21:49 -0500

    first commit

---
+++ C-Prac-2.1.2.14-LAB-Operators-Part3.cpp
@@ -0,0 +1,35 @@
+//Scenario
+//Check the program in the editor.Find all possible compilation errors and logic errors.Fix them, but do not change any numeric values.
+//
+//Your version of the program must print the same result as the expected output.Before you use the compiler, try to find the errors only by manual code analysis.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Code
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	float tenValue = 2 3 4;
+//	float twelveValue = 2 2.5 2 3.5;
+//	printf("The value of ten is: %f\n", tenValue);
+//	printf("The value of twelve is: %f\n", twelveValue);
+//	return 0;
+//}
+//
+//Expected output
+//The value of ten is : 10.000000
+//The value of twelve is : 12.000000
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+	float	tenValue = 2 * 3 + 4;
+	float twelveValue = 2 * 2.5 + 2 * 3.5;
+	printf("The value of ten is: %f\n", tenValue);
+	printf("The value of twelve is: %f\n", twelveValue);
+	return 0;
+}
